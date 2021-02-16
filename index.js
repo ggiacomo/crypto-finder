de12fa2a29851b5385a6e4d4842993d5fef547c4
Jack <jack@top.station>
2021-02-16 11:35:41 +0100

    merged exchanges calls and UI

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const ath1 = require("./data/ath1.json");
 const unlistedBinance = require("./data/unlisted-binance.json");
 const unlistedCoinbase = require("./data/unlisted-coinbase.json");
+const app = express();
+
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 const {
   ping,
@@ -10,12 +15,27 @@ const {
   coinsNotListedYetOn,
   coinsNotListedYetOnBinance,
 } = require("./coingecko-api");
-const app = express();
 
-app.get("/", async (req, res) => {
-  const result = await ping();
-  res.json(result);
-});
+
+const toExclude = [
+  "usdt",
+  "usdc",
+  "cro",
+  "cel",
+  "bnb",
+  " busd",
+  "cusdc",
+  "bsv",
+  "ust",
+];
+
+app.get('/', async (req, res) => {
+  try {
+    res.render('index')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.get("/ath", async (req, res) => {
   const result = await coinsNotToAthYet();
@@ -42,17 +62,16 @@ app.get("/ath-data", async (req, res) => {
   res.json(result);
 });
 
-app.get("/unlisted-binance-coins", async (req, res) => {
-  const coins = await coinsNotListedYetOn("binance");
-  const result = coins
-    .filter(onlyUnique)
-    .filter(
-      (c) =>
-        c.market_data.market_cap_rank && c.market_data.market_cap_rank < 250
-    )
-    .map((c) => c.name);
-  res.json(result);
-});
+// app.get("/unlisted-binance-coins", async (req, res) => {
+  // const coins = await coinsNotListedYetOn("binance");
+  // const result = coins.filter(onlyUnique);
+  // .filter(
+  //   (c) =>
+  //     c.market_data.market_cap_rank && c.market_data.market_cap_rank < 250
+  // )
+  // .map((c) => c.name);
+  // res.json(result);
+// });
 
 // app.get("/unlisted-binance-data", async (req, res) => {
 //   const result = unlistedBinance
@@ -69,15 +88,46 @@ app.get("/unlisted-binance-coins", async (req, res) => {
 //   res.json(result);
 // });
 
-app.get("/unlisted-coinbase-coins", async (req, res) => {
-  const coins = await coinsNotListedYetOn("gdax");
+// app.get("/unlisted-coinbase-coins", async (req, res) => {}
+app.get("/unlisted-coins", async (req, res) => {
+  let binanceCoins = await coinsNotListedYetOn("binance");
+  binanceCoins = binanceCoins.map((c)=> Object.assign({}, c, {exchange: 'Binance'}))
+  let coinbaseCoins = await coinsNotListedYetOn("gdax");
+  coinbaseCoins = coinbaseCoins.map((c)=> Object.assign({}, c, {exchange: 'Coinbase'}))
+  const coins = [...new Set([...binanceCoins, ...coinbaseCoins])];
   const result = coins
     .filter(onlyUnique)
-    .filter(
-      (c) =>
-        c.market_data.market_cap_rank && c.market_data.market_cap_rank < 250
-    )
-    .map((c) => c.name);
+    // .filter(
+    //   (c) =>
+    //     c.market_data.market_cap_rank && c.market_data.market_cap_rank < 2000
+    // )
+    .filter((c) => !toExclude.includes(c.symbol))
+    .map((c) => {
+      const toATH = c.ath - c.current_price;
+      const toATHPercentage = toATH/(c.ath/100);
+      let action = "-";
+      if (toATH > 0) {
+        action = "-";
+      }
+      // console.log(c)
+      return {
+        id: c.id,
+        image: c.image,
+        symbol: c.symbol,
+        name: c.name,
+        current_price: c.current_price,
+        market_cap_rank: c.market_cap_rank,
+        ath: c.ath,
+        price_change_percentage_24h: Math.round((c.price_change_percentage_24h + Number.EPSILON) * 100) / 100 ,
+        price_change_percentage_7d_in_currency:
+        Math.round((c.price_change_percentage_7d_in_currency + Number.EPSILON) * 100) / 100,
+        exchange : c.exchange,
+        toATH,
+        toATHPercentage : Math.round((toATHPercentage + Number.EPSILON) * 100) / 100,
+        action,
+      };
+    })
+    .sort((a, b) => a.market_cap_rank - b.market_cap_rank);
   res.json(result);
 });
 
